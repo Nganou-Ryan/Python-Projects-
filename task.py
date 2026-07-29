09b6d536f52b858176c18e4b25cef3bdaef65d03
Nganou-Ryan <136168218+Nganou-Ryan@users.noreply.github.com>
2026-07-29 15:01:46 -0400

    Add files via upload

---
+++ task.py
@@ -0,0 +1,6 @@
+print("Hello Everyone you're welcome in the wonder generator name")
+city_name=input("What's a name of your city?\n")
+pet_name=input("What's your pet name?\n")
+name_city = city_name + ' ' + pet_name
+print('your city name is' + ' ' + name_city)
+
